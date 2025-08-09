436d61d1d56307206342e4d6b5ef2b96f9193b87
Jaden <69062297+ExxtraV@users.noreply.github.com>
2025-08-08 21:52:03 -0400

    fix: import useMDXComponent from next-contentlayer2/hooks

--- components/mdx-content.tsx
+++ components/mdx-content.tsx
@@ -1,13 +1,17 @@
 'use client';
+
 import * as runtime from 'react/jsx-runtime';
-import { useMDXComponent } from 'next-contentlayer/hooks';
+import { useMDXComponent } from 'next-contentlayer2/hooks';
 import Link from 'next/link';
 
 const components = {
   a: (props: any) => <a {...props} />,
-  // Add custom shortcodes as you like:
+  // Shortcodes you can use inside MDX:
   Spoiler: ({ children }: { children: React.ReactNode }) => (
-    <details><summary>Spoiler</summary><div>{children}</div></details>
+    <details>
+      <summary>Spoiler</summary>
+      <div>{children}</div>
+    </details>
   ),
   WikiLink: ({ slug, children }: { slug: string; children: React.ReactNode }) => (
     <Link href={`/wiki/${slug}`}>{children}</Link>
